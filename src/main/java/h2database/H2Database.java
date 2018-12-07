32a6bff1a8b32be8c72257562237eb2c44cedbdf
tanu02 <tanubatra02@gmail.com>
2018-12-07 23:26:20 +0530

    @component + @scope + @Autowired + @qualifier
    @RestController + @RequestMapping + @RequestParam
    
    Configured h2 database (main work is in application.properties file)

---
+++ src/main/java/h2database/H2Database.java
@@ -0,0 +1,12 @@
+package h2database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2Database {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2Database.class, args);
+	}
+}
